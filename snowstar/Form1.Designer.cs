2365dc3044d15719593e4db9f7061ad9a020a182
KorlMaster <88134646+KorlMaster@users.noreply.github.com>
2025-01-21 18:26:44 +0100

    Add files via upload

---
+++ snowstar/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace snowstar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_login = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ServerStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CheckingStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_USERNAME = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_PASSWORD = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_login
+            // 
+            this.button_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_login.Location = new System.Drawing.Point(9, 329);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(85, 30);
+            this.button_login.TabIndex = 0;
+            this.button_login.Text = "Login";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.ServerStatusLabel,
+            this.CheckingStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 366);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(257, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Server:";
+            // 
+            // ServerStatusLabel
+            // 
+            this.ServerStatusLabel.Name = "ServerStatusLabel";
+            this.ServerStatusLabel.Size = new System.Drawing.Size(22, 17);
+            this.ServerStatusLabel.Text = ".....";
+            // 
+            // CheckingStatusLabel
+            // 
+            this.CheckingStatusLabel.Name = "CheckingStatusLabel";
+            this.CheckingStatusLabel.Size = new System.Drawing.Size(22, 17);
+            this.CheckingStatusLabel.Text = ",,,,,";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-53, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "___________________________________________________________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-52, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(361, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "___________________________________________________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "UserID:";
+            // 
+            // textBox_USERNAME
+            // 
+            this.textBox_USERNAME.Location = new System.Drawing.Point(9, 237);
+            this.textBox_USERNAME.Name = "textBox_USERNAME";
+            this.textBox_USERNAME.Size = new System.Drawing.Size(236, 20);
+            this.textBox_USERNAME.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password:";
+            // 
+            // textBox_PASSWORD
+            // 
+            this.textBox_PASSWORD.Location = new System.Drawing.Point(9, 283);
+            this.textBox_PASSWORD.Name = "textBox_PASSWORD";
+            this.textBox_PASSWORD.Size = new System.Drawing.Size(236, 20);
+            this.textBox_PASSWORD.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::snowstar.Properties.Resources._670f5a0172b90570b1c21dab_kofi_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(114, 326);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(134, 36);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::snowstar.Properties.Resources.trayicon;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(234, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 388);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox_PASSWORD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_USERNAME);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_login);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SnowStar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel ServerStatusLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_USERNAME;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_PASSWORD;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripStatusLabel CheckingStatusLabel;
+    }
+}
+
---
+++ snowstar/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// LAUNCHER BY KARLMASTER
+
+namespace snowstar
+{
+    public partial class Form1 : Form
+    {
+        bool launch_ok = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // Load settings from config
+            string kofiValue = ConfigurationManager.AppSettings["kofi"];
+            string kofiLink = ConfigurationManager.AppSettings["kofi_link"];
+
+            if (kofiValue == "1")
+            {
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                pictureBox2.Visible = false;
+            }
+
+            button_login.Enabled = false;
+            textBox_USERNAME.Focus();
+            this.ActiveControl = textBox_USERNAME;
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.WindowState = FormWindowState.Normal;
+
+            textBox_PASSWORD.UseSystemPasswordChar = true;
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+            ServerStatusLabel.Text = string.Empty;
+            CheckingStatusLabel.Text = string.Empty;
+
+            string serverAddress = LoadServerAddress("server.cfg");
+            int port = 10002;
+
+            if (string.IsNullOrEmpty(serverAddress))
+            {
+                ServerStatusLabel.ForeColor = Color.DarkRed;
+                ServerStatusLabel.Text = "Failed to load server address!";
+                return;
+            }
+
+            ServerStatusLabel.Text = "Checking...";
+
+            bool isOnline = await CheckServerStatusAsync(serverAddress, port);
+
+            if (isOnline)
+            {
+                ServerStatusLabel.ForeColor = Color.DarkGreen;
+                ServerStatusLabel.Text = "ONLINE";
+                launch_ok = true;
+                button_login.Enabled = true;
+            }
+            else
+            {
+                ServerStatusLabel.ForeColor = Color.DarkRed;
+                ServerStatusLabel.Text = "OFFLINE";
+            }
+        }
+
+        private string LoadServerAddress(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("server.cfg not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    if (line.Trim().StartsWith("SERVER_GAME"))
+                    {
+                        int startIndex = line.IndexOf('"') + 1;
+                        int endIndex = line.LastIndexOf('"');
+                        if (startIndex > 0 && endIndex > startIndex)
+                        {
+                            return line.Substring(startIndex, endIndex - startIndex);
+                        }
+                    }
+                }
+
+                MessageBox.Show("SERVER_GAME not found in server.cfg!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to read server.cfg: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (launch_ok == true)
+                    button_login_Click(this, EventArgs.Empty);
+            }
+        }
+
+        private async Task<bool> CheckServerStatusAsync(string serverAddress, int port)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    var connectTask = client.ConnectAsync(serverAddress, port);
+                    var timeoutTask = Task.Delay(10000);
+
+                    var completedTask = await Task.WhenAny(connectTask, timeoutTask);
+                    if (completedTask == timeoutTask)
+                    {
+                        return false;
+                    }
+
+                    return client.Connected;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static string sha256(string password)
+        {
+            using (var crypt = new System.Security.Cryptography.SHA256Managed())
+            {
+                byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(crypto).Replace("-", "").ToLowerInvariant();
+            }
+        }
+
+        private async void button_login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox_USERNAME.Text) || string.IsNullOrWhiteSpace(textBox_PASSWORD.Text))
+            {
+                MessageBox.Show("Please fill in both username and password.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            button_login.Enabled = false;
+            CheckingStatusLabel.Text = "Verifying files...";
+
+            bool filesValid = await Task.Run(() => VerifyPatchlist());
+
+            if (!filesValid)
+            {
+                MessageBox.Show("File integrity check failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button_login.Enabled = true;
+                return;
+            }
+
+            try
+            {
+                string username = textBox_USERNAME.Text;
+                string password = textBox_PASSWORD.Text;
+
+                StartGame(username, password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button_login.Enabled = true;
+            }
+        }
+
+        private void StartGame(string username, string password)
+        {
+            string gamePath = Path.Combine(Directory.GetCurrentDirectory(), "game.exe");
+
+            if (!File.Exists(gamePath))
+            {
+                MessageBox.Show("game.exe not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                string arguments = $"-karlmaster_to {(char)'a'}{sha256(password)}{username}";
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = gamePath,
+                    Arguments = arguments,
+                    UseShellExecute = true
+                });
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to start the game: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool VerifyPatchlist()
+        {
+            try
+            {
+                string[] patchlist = Encoding.UTF8.GetString(Properties.Resources.files)
+                    .Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                int totalFiles = patchlist.Length;
+                int checkedFiles = 0;
+
+                foreach (string entry in patchlist)
+                {
+                    string[] parts = entry.Split('|');
+                    if (parts.Length != 2)
+                    {
+                        MessageBox.Show("Invalid entry in files checking list", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+
+                    string relativePath = parts[0];
+                    string expectedHash = parts[1];
+
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), relativePath);
+                    if (!File.Exists(filePath))
+                    {
+                        MessageBox.Show($"File not found: {relativePath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+
+                    string actualHash = ComputeFileHash(filePath);
+                    if (!actualHash.Equals(expectedHash, StringComparison.OrdinalIgnoreCase))
+                    {
+                        MessageBox.Show($"Hash mismatch for file: {relativePath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+
+                    checkedFiles++;
+                    float progress = (float)checkedFiles / totalFiles * 100;
+                    CheckingStatusLabel.Text = $" | Checking files.. {progress:F2}%";
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error verifying patchlist: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private string ComputeFileHash(string filePath)
+        {
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            using (var stream = File.OpenRead(filePath))
+            {
+                byte[] hash = sha256.ComputeHash(stream);
+                return BitConverter.ToString(hash).Replace("-", string.Empty).ToLowerInvariant();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            string kofiLink = ConfigurationManager.AppSettings["kofi_link"] ?? "https://ko-fi.com/karlmaster";
+            Process.Start(new ProcessStartInfo(kofiLink) { UseShellExecute = true });
+        }
+    }
+}
